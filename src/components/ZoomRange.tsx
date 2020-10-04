2c3a8c834951bebb47e1bd97324a13418366511f
Matt Orchard <morch028@uottawa.ca>
2020-10-04 12:34:01 -0400

    Updates zoom range to include spinner buttons

--- src/components/ZoomRange.tsx
+++ src/components/ZoomRange.tsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import useControlScroll from "../hooks/useControlScroll";
+import { faSearchMinus, faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./ZoomRange.css";
+
+const MIN = -2;
+const MAX = 0;
+const STEP = 0.1;
 
 const ZoomRange: React.FC<{
   zoom: number;
   onZoomChange: (zoom: number) => void;
 }> = ({ zoom, onZoomChange }) => {
-  const [value, setValue] = React.useState(() => Math.log10(zoom));
+  const [value, setRawValue] = useState(() => Math.log10(zoom));
 
-  const setBothValues = (value: number) => {
-    setValue(value);
+  const setValue = (value: number) => {
+    setRawValue(value);
     onZoomChange(Math.pow(10, value));
   };
 
-  useControlScroll((scrollDelta) => setBothValues(value + scrollDelta / 1000));
+  useControlScroll((scrollDelta) => setValue(value + scrollDelta / 1000));
 
   return (
-    <label>
-      Zoom
-      <span>
-        -
-        <input
-          type="range"
-          min={-4}
-          max={0}
-          step={0.1}
-          value={value}
-          onChange={(event) => setBothValues(Number(event.currentTarget.value))}
-        />
-        +
-      </span>
-    </label>
+    <div className="zoom-range" title="Timeline zoom" role="group">
+      <button
+        title="Zoom out"
+        onClick={() => setValue(Math.max(MIN, value - STEP))}
+      >
+        <FontAwesomeIcon icon={faSearchMinus} />
+      </button>
+      <input
+        type="range"
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={value}
+        onChange={(event) => setValue(Number(event.currentTarget.value))}
+      />
+      <button
+        title="Zoom in"
+        onClick={() => setValue(Math.min(MAX, value + STEP))}
+      >
+        <FontAwesomeIcon icon={faSearchPlus} />
+      </button>
+    </div>
   );
 };
 
