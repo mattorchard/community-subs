9cc33db60bda3db732500133b723dd609d100728
Matt Orchard <morch028@uottawa.ca>
2020-10-06 19:57:44 -0400

    Changes initial transcript name

--- src/contexts/TranscriptContext.tsx
+++ src/contexts/TranscriptContext.tsx
@@ -64,7 +64,7 @@ export const TranscriptContextProvider: React.FC = ({ children }) => {
       createTranscript: async () => {
         const savedTranscript = await putTranscript({
           id: uuidV4(),
-          name: "New Transcript",
+          name: "Untitled Transcript",
           createdAt: new Date(),
           accessedAt: new Date(),
         });
